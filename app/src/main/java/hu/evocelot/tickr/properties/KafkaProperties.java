087f01581560966c6c550c01a6b30b7f32046716
DanisovszkyMark <dmarkprog@gmail.com>
2025-05-09 22:35:14 +0200

    Improve properties handling

--- app/src/main/java/hu/evocelot/tickr/properties/KafkaProperties.java
+++ app/src/main/java/hu/evocelot/tickr/properties/KafkaProperties.java
@@ -1,46 +1,42 @@
 package hu.evocelot.tickr.properties;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Properties class for reading Kafka-related environment variables.
  * 
  * @author mark.danisovszky
  */
-@Component
+@Configuration
+@ConfigurationProperties(prefix = "kafka")
 public class KafkaProperties {
 
-    @Value("${KAFKA_ENABLED:false}")
-    private String kafkaEnabled;
+    private String enabled;
+    private String url;
+    private String groupId;
 
-    @Value("${KAFKA_URL:}")
-    private String kafkaUrl;
-
-    @Value("${KAFKA_GROUP_ID:}")
-    private String kafkaGroupId;
-
-    public String getKafkaEnabled() {
-        return kafkaEnabled;
+    public String getEnabled() {
+        return enabled;
     }
 
-    public void setKafkaEnabled(String kafkaEnabled) {
-        this.kafkaEnabled = kafkaEnabled;
+    public void setEnabled(String kafkaEnabled) {
+        this.enabled = kafkaEnabled;
     }
 
-    public String getKafkaUrl() {
-        return kafkaUrl;
+    public String getUrl() {
+        return url;
     }
 
-    public void setKafkaUrl(String kafkaUrl) {
-        this.kafkaUrl = kafkaUrl;
+    public void setUrl(String kafkaUrl) {
+        this.url = kafkaUrl;
     }
 
-    public String getKafkaGroupId() {
-        return kafkaGroupId;
+    public String getGroupId() {
+        return groupId;
     }
 
-    public void setKafkaGroupId(String kafkaGroupId) {
-        this.kafkaGroupId = kafkaGroupId;
+    public void setGroupId(String kafkaGroupId) {
+        this.groupId = kafkaGroupId;
     }
 }
